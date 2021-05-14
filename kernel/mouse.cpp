ef643558e61f8ca0eded098290abfa3f8bcc2b3a
happou31 <happo31@gmail.com>
2021-05-15 02:51:45 +0900

    fix: 参照渡しをするように

--- kernel/mouse.cpp
+++ kernel/mouse.cpp
@@ -1,5 +1,7 @@
 #include "mouse.hpp"
 
+#include "logger.hpp"
+
 namespace {
 const char mouse_cousor_shape[kMouseCursorHeight][kMouseCursorWidth + 1] = {
     "@              ", "@@             ", "@.@            ", "@..@           ",
@@ -11,15 +13,15 @@ const char mouse_cousor_shape[kMouseCursorHeight][kMouseCursorWidth + 1] = {
 };
 }
 
-void DrawMouseCursor(PixelWriter* writer, Vector2D<int> position) {
+void DrawMouseCursor(PixelWriter& writer, Vector2D<int> position) {
   for (int dy = 0; dy < kMouseCursorHeight; ++dy) {
     for (int dx = 0; dx < kMouseCursorWidth; ++dx) {
       if (mouse_cousor_shape[dy][dx] == '@') {
-        writer->Write({0, 0, 0}, position.x + dx, position.y + dy);
+        writer.Write({0, 0, 0}, position + Vector2D<int>{dx, dy});
       } else if (mouse_cousor_shape[dy][dx] == '.') {
-        writer->Write({255, 255, 255}, position.x + dx, position.y + dy);
+        writer.Write({255, 255, 255}, position + Vector2D<int>{dx, dy});
       } else {
-        writer->Write(kMouseTransparentColor, position + Vector2D<int>{dx, dy});
+        writer.Write(kMouseTransparentColor, position + Vector2D<int>{dx, dy});
       }
     }
   }
--- kernel/mouse.hpp
+++ kernel/mouse.hpp
@@ -6,4 +6,4 @@ const int kMouseCursorWidth = 15;
 const int kMouseCursorHeight = 24;
 const PixelColor kMouseTransparentColor{0, 0, 1};
 
-void DrawMouseCursor(PixelWriter* writer, Vector2D<int> position);
+void DrawMouseCursor(PixelWriter& writer, Vector2D<int> position);
