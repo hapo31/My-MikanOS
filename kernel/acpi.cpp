d8463b5c819125724026c0b88be6ee4d8a4e082c
happou31 <happo31@gmail.com>
2021-05-17 23:07:43 +0900

    feat: 12.1

--- kernel/acpi.cpp
+++ kernel/acpi.cpp
@@ -46,9 +46,52 @@ bool RSDP::IsValid() const {
   return true;
 }
 
+bool DescriptionHeader::IsValid(const char* expected_signature) const {
+  if (strncmp(this->signature, expected_signature, 4) != 0) {
+    Log(kDebug, "invalid signature: %.4\n", this->signature);
+    return false;
+  }
+  if (auto sum = SumBytes(this, this->length); sum != 0) {
+    Log(kDebug, "sum of %u bytes must be 0:\n", this->length, sum);
+    return false;
+  }
+  return true;
+}
+
+const DescriptionHeader& XSDT::operator[](size_t i) const {
+  auto entiries = reinterpret_cast<const uint64_t*>(&this->header + 1);
+  return *reinterpret_cast<const DescriptionHeader*>(entiries[i]);
+}
+
+size_t XSDT::Count() const {
+  return (this->header.length - sizeof(DescriptionHeader)) / sizeof(uint64_t);
+}
+
+const FADT* fadt = nullptr;
+
 void Initialize(const RSDP& rsdp) {
   if (!rsdp.IsValid()) {
     Log(kError, "RSDP is not valid\n");
+    exit(1);
+  }
+
+  const XSDT& xsdt = *rsdp.GetXSDTAddr();
+  if (xsdt.header.IsValid("XSDT")) {
+    Log(kError, "RSDP is not valid(expect: %s, actual: %s)\n", "XSDT",
+        xsdt.header.signature);
+    exit(1);
+  }
+
+  for (int i = 0; i < xsdt.Count(); ++i) {
+    const auto& entry = xsdt[i];
+    if (entry.IsValid("FACP")) {
+      fadt = entry.ToFADTAddr();
+      break;
+    }
+  }
+
+  if (fadt == nullptr) {
+    Log(kError, "FADT is not found\n");
     exit(1);
   }
 }
--- kernel/acpi.hpp
+++ kernel/acpi.hpp
@@ -1,18 +1,58 @@
 #pragma once
+#include <cstddef>
 #include <cstdint>
 
 namespace acpi {
+
+struct FADT;
+
+struct DescriptionHeader {
+  char signature[4];
+  uint32_t length;
+  uint8_t revisition;
+  uint8_t checksum;
+  char oem_id[6];
+  char oem_table_id[8];
+  uint32_t oem_revisition;
+  uint32_t creator_id;
+  uint32_t creator_revisition;
+  bool IsValid(const char* expected_signature) const;
+  auto ToFADTAddr() const { return reinterpret_cast<const FADT*>(this); }
+} __attribute__((packed));
+
+struct XSDT {
+  DescriptionHeader header;
+  const DescriptionHeader& operator[](size_t i) const;
+  size_t Count() const;
+} __attribute__((packed));
+
 struct RSDP {
   char signature[8];
   uint8_t checksum;
   char oem_id[6];
   uint8_t revision;
+  uint32_t rsdt_address;
   uint32_t length;
   uint64_t xsdt_address;
   uint8_t extended_checksum;
   char reserved[3];
+
   bool IsValid() const;
+  auto GetXSDTAddr() const {
+    return reinterpret_cast<const XSDT*>(xsdt_address);
+  }
 } __attribute__((packed));
+
+struct FADT {
+  DescriptionHeader header;
+  char reserved1[76 - sizeof(header)];
+  uint32_t pm_tmr_blk;
+  char reserved2[112 - 80];
+  uint32_t flags;
+  char reserved3[276 - 116];
+} __attribute__((packed));
+
+extern const FADT* fadt;
 
 void Initialize(const RSDP& rsdp);
 }  // namespace acpi
