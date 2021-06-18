70b921b6ec8ddda0f8666df5deadde1b854d2832
happou31 <happo31@gmail.com>
2021-06-18 23:55:31 +0900

    fix: apps/ をディスクにコピーするように

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+for MK in $(ls apps/*/Makefile)
+do
+  APP_DIR=$(dirname $MK)
+  APP=$(basename $APP_DIR)
+  make ${MAKE_OPTS:-} -C $APP_DIR $APP
+done
+
 pushd kernel
 
 if [ $1 = "clean" ]; then
--- run_qemu_my_kernel.sh
+++ run_qemu_my_kernel.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-
 sudo ./devenv/run_qemu.sh ./edk2/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi ./kernel/kernel.elf
